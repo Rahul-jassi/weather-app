5e61e2ba74000b4f5d9544d1a4555eef20313699
rahuljassi <jassirahul11@gmail.com>
2020-04-18 21:36:08 +0530

    my weather app

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('starting')
+
+setTimeout( ()=>{
+    console.log('3 secound time')
+},3000)
+setTimeout( ()=>{
+    console.log('0 secound time')
+},000)
+
+
+
+
+console.log('finishing')
